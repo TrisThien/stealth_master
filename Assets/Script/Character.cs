c096e86e0429f52ab83921efcffac8bfd76a580e
TrisThien <professor.no90@gmail.com>
2022-01-27 02:17:03 +0700

    ap dung FSM va tao animation

---
+++ Assets/Script/Character.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    internal void AnimateIdle()
+    {
+        animator.CrossFade("Idle", 0.1f);
+    }
+    internal void AnimateWalk()
+    {
+        animator.CrossFade("Sneak", 0.1f);
+        
+    }
+    internal void AnimateRun()
+    {
+        animator.CrossFade("Run", 0.1f);
+        
+    }
+    internal void AnimateAttack()
+    {
+        animator.CrossFade("Attack", 0.1f);
+        
+    }
+    internal void AnimateWin()
+    {
+        animator.CrossFade("Win", 0.1f);
+
+    }
+    internal void AnimateDie()
+    {
+        
+    }
+}
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,24 +1,155 @@
+using System;
 using Funzilla;
 using UnityEngine;
 using UnityEngine.AI;
 
 // FSM cho Player: https://gitmind.com/app/doc/4637857181
 
-namespace Script
-{
     public class Player : MonoBehaviour
     {
         private const float Speed = 10f;
         [SerializeField] private DynamicJoystick joystick;
         [SerializeField] private NavMeshAgent agent;
+        [SerializeField] private Character character;
+        
+        private const float WalkSpeed = 9.99f;
+        private const float AttackRange = 3f;
+        private const float AttackTime = 0.7f;
+        
+        private float _attackTime;
+        private float _hp;
+
+        private Enemy[] _enemies;
+        private Exit _exit;
+        
+        enum States
+        {
+            Idle, Walk, Run, Attack, Win, Die
+        }
+
+        private States _state = States.Idle;
+
         private void Start()
         {
             Application.targetFrameRate = 60;
+
+            _enemies = FindObjectsOfType<Enemy>();
+            _exit = FindObjectOfType<Exit>();
+
+            _hp = 50f;
         }
+
         private void Update()
         {
             var v = joystick.Direction * Speed;
             agent.velocity = new Vector3(v.x, 0, v.y);
+
+            if (_state != States.Die && Vector3.Distance(transform.position, _exit.transform.position) <= 1f)
+            {
+                ChangeState(States.Win);
+                return;
+            }
+
+            if (_hp <= 0)
+            {
+                ChangeState(States.Die);
+            }
+            
+            switch (_state)
+            {
+                case States.Idle:
+                    if(TryAttack()) return;
+                    if (agent.velocity.magnitude > 0) ChangeState(States.Walk);
+                    break;
+                case States.Walk:
+                    if(TryAttack()) return;
+                    if(agent.velocity.magnitude > WalkSpeed) ChangeState(States.Run);
+                    else if(agent.velocity.magnitude <= 0) ChangeState(States.Idle);
+                    break;
+                case States.Run:
+                    if(TryAttack()) return;
+                    if(agent.velocity.magnitude <= WalkSpeed) ChangeState(States.Walk);
+                    break;
+                case States.Attack:
+                    _attackTime -= Time.deltaTime;
+                    if (_attackTime <= 0)
+                    {
+                        ChangeState(States.Walk);
+                    }
+                    break;
+                case States.Win:
+                    break;
+                case States.Die:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private bool TryAttack()
+        {
+            foreach (var e in _enemies)
+            {
+                if (Vector3.Distance(e.transform.position, transform.position) < AttackRange)
+                {
+                    ChangeState(States.Attack);
+                    return true;
+                }
+            }
+            return false;
+        }
+        private void ChangeState(States newState)
+        {
+            if (newState == _state) return;
+            ExitCurrentState();
+            _state = newState;
+            EnterNewState();
+        }
+
+        private void EnterNewState()
+        {
+            switch (_state)
+            {
+                case States.Idle:
+                    character.AnimateIdle();
+                    break;
+                case States.Walk:
+                    character.AnimateWalk();
+                    break;
+                case States.Run:
+                    character.AnimateRun();
+                    break;
+                case States.Attack:
+                    character.AnimateAttack();
+                    _attackTime = AttackTime;
+                    break;
+                case States.Win:
+                    break;
+                case States.Die:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void ExitCurrentState()
+        {
+            switch (_state)
+            {
+                case States.Idle:
+                    break;
+                case States.Walk:
+                    break;
+                case States.Run:
+                    break;
+                case States.Attack:
+                    break;
+                case States.Win:
+                    break;
+                case States.Die:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
     }
-}
