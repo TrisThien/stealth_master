a73d9cc4b862bb428c97cb3087b8e405359e21c8
Le Tri Thien <professor.no90@gmail.com>
2022-02-16 23:54:37 +0700

    install dotween

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -2,22 +2,25 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private Player _player;
-    private Vector3 _distanceGap;
-    private void Start()
-    {
-        _player = FindObjectOfType<Player>();
-        _distanceGap = transform.position - _player.transform.position;
-    }
-    private void Update()
-    {
-        CameraMove();
-    }
-    private void CameraMove()
-    {
-        var camMove = _player.transform.position + _distanceGap;
-        
-        camMove = new Vector3(Mathf.Clamp(camMove.x, -4f, 4f), camMove.y, camMove.z);
-        transform.position = camMove;
-    }
+	private Player _player;
+	private Vector3 _distanceGap;
+
+	private void Start()
+	{
+		_player = FindObjectOfType<Player>();
+		_distanceGap = transform.position - _player.transform.position;
+	}
+
+	private void Update()
+	{
+		CameraMove();
+	}
+
+	private void CameraMove()
+	{
+		var camMove = _player.transform.position + _distanceGap;
+
+		camMove = new Vector3(Mathf.Clamp(camMove.x, -4f, 4f), camMove.y, camMove.z);
+		transform.position = camMove;
+	}
 }
--- Assets/Script/Enemy/EnemyPatrol.cs
+++ Assets/Script/Enemy/EnemyPatrol.cs
@@ -2,21 +2,22 @@ using UnityEngine;
 
 public class EnemyPatrol : IEnemyState
 {
-    private readonly Player _player = new Player();
-    private const float EnemyView = 3f;
-    public IEnemyState ChangeState(Enemy enemy)
-    {
-        if(!SeePlayer(enemy)) Patrol(enemy);
-        return enemy.EnemyChase;
-    }
+	private readonly Player _player = new Player();
+	private const float EnemyView = 3f;
 
-    private void Patrol(Enemy enemy)
-    {
-        // TODO
-    }
+	public IEnemyState ChangeState(Enemy enemy)
+	{
+		if (!SeePlayer(enemy)) Patrol(enemy);
+		return enemy.EnemyChase;
+	}
 
-    private bool SeePlayer(Enemy enemy)
-    {
-        return Vector3.Distance(enemy.transform.position, _player.transform.position) <= EnemyView;
-    }
+	private void Patrol(Enemy enemy)
+	{
+		// TODO
+	}
+
+	private bool SeePlayer(Enemy enemy)
+	{
+		return Vector3.Distance(enemy.transform.position, _player.transform.position) <= EnemyView;
+	}
 }
